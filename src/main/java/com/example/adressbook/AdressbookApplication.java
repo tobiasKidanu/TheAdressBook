49aa76b248b4964075c5da2728d3d7f95cef2d7f
Grug Ug <e94doclrd@hotmail.com>
2021-03-22 17:39:17 +0100

    Added Global CORS config in application

--- src/main/java/com/example/adressbook/AdressbookApplication.java
+++ src/main/java/com/example/adressbook/AdressbookApplication.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AdressbookApplication {
@@ -15,5 +18,17 @@ public class AdressbookApplication {
         SpringApplication.run(AdressbookApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/delete").allowedOrigins("http://localhost:4200");
+                registry.addMapping("/put").allowedOrigins("http://localhost:4200");
+                registry.addMapping("/post").allowedOrigins("http://localhost:4200");
+                registry.addMapping("/get").allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
 
 }
