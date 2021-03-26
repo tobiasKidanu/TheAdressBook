6a0da301fa782cdc3cc127bc806cb8c058237278
Douglas Clifford <douglas.clifford@gmp-systems.com>
2021-03-26 11:39:09 +0100

    Add submit component

---
+++ src/web/src/app/address-submit/address-submit.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddressSubmitComponent } from './address-submit.component';
+
+describe('AddressSubmitComponent', () => {
+  let component: AddressSubmitComponent;
+  let fixture: ComponentFixture<AddressSubmitComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AddressSubmitComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddressSubmitComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/web/src/app/address-submit/address-submit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-address-submit',
+  templateUrl: './address-submit.component.html',
+  styleUrls: ['./address-submit.component.css']
+})
+export class AddressSubmitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
