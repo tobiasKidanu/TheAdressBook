560449c32a35c0cc536640bbb350a2a4737bfc4a
Tobias Mindell Kidanu <tobias.mindellkidanu@gmp-systems.com>
2021-03-26 10:48:49 +0100

    change endpoints in controller

--- src/web/src/app/contact.service.ts
+++ src/web/src/app/contact.service.ts
@@ -19,22 +19,22 @@ export class ContactService {
   addContact(name: string, adress: string): Observable<any>{
     const newContact = {name: name, adress: adress};
     //   this.httpClient.post(this.url + '/create',newContact).subscribe(
-    return this.httpClient.post<Promise<Contact>>(this.url + '/create',newContact);
+    return this.httpClient.post<Promise<Contact>>(this.url + '/',newContact);
   }
 
   getContacts(): Observable<any> {
-    return this.httpClient.get(this.url + '/all');
+    return this.httpClient.get(this.url + '/');
   }
 
   deleteContact(contactToDelete: Contact): Observable<any> {
     let contactId = contactToDelete.id;
     console.log("delete contact with id " + contactId);
-    return this.httpClient.delete(this.url + '/delete/' + contactId);
+    return this.httpClient.delete(this.url + '/' + contactId);
   }
 
   updateContact(contactToEdit: Contact){
     let contactId = contactToEdit.id;
     console.log("Editing contact" + contactId);
-    this.httpClient.put(this.url + '/update/' + contactId,contactToEdit).toPromise().then();
+    this.httpClient.put(this.url + '/' + contactId,contactToEdit).toPromise().then();
   }
 }
