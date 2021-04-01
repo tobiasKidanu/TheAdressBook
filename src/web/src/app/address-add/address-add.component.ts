ebe6f738145bcbfcab70912a6301be9f6abbcd5b
Douglas Clifford <douglas.clifford@gmp-systems.com>
2021-04-01 09:33:19 +0200

    Renamed selector and adress update event

--- src/web/src/app/address-add/address-add.component.ts
+++ src/web/src/app/address-add/address-add.component.ts
@@ -4,13 +4,13 @@ import { Contact} from "../contact";
 import {AddressListComponent} from "../address-list/address-list.component";
 
 @Component({
-  selector: 'app-address-submit',
+  selector: 'app-address-add',
   templateUrl: './address-add.component.html',
   //styleUrls: ['./address-add.component.css']
 })
 export class AddressAddComponent implements OnInit {
 
-  @Output() updateEvent = new EventEmitter<any>();
+  @Output() addedAddress = new EventEmitter<any>();
 
   private contactService: ContactService;
 
@@ -24,7 +24,7 @@ export class AddressAddComponent implements OnInit {
   addContact(name: string, adress: string) {
     this.contactService.addContact(name, adress).subscribe(
       response => {
-        this.updateEvent.emit();
+        this.addedAddress.emit();
       }
     );
   }
